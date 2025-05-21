b0159163aa652818687e96f15201bc33c97735f6
Stephen Doe <donsimon505@gmail.com>
2025-05-21 03:03:30 +0000

    Fix

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 let langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let x;
 
-for(x = 0; x < langs.length; x++){
+for (x = 0; x < langs.length; x++){
     console.log(langs[x]);
 };
