c4e7ce25775cbb2e2eba9801975cd274d29c919a
Stephen Doe <donsimon505@gmail.com>
2025-05-21 02:48:11 +0000

    To Integer Script Added

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+const {argv} = require('node:process');
+let args = argv.slice(2);
+
+let numArg = parseInt(args);
+
+if (isNaN(numArg)){
+    console.log('Not a number')
+} else{
+    console.log('My Number : ' + numArg);
+}
